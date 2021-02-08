c257400fa819f8a5f904904c335a0c2c2fd46b75
Santi Espinosa <saigesp@gmail.com>
2021-02-08 16:27:41 +0100

    Add OJITO social networks

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,6 +6,20 @@
       <router-link :to="{name: 'covid-report'}">Informe COVID</router-link>
       <router-link :to="{name: 'contact'}">Contacto</router-link>
     </div>
+    <div class="footer__social">
+      <a href="https://www.instagram.com/o.j.i.t.o/">
+        <img src="/img/icons/instagram.png" alt="Instagram de OJITO">
+        <span>o.j.i.t.o</span>
+      </a>
+      <a href="https://twitter.com/OJITO_PW">
+        <img src="/img/icons/twitter.png" alt="Twitter de OJITO">
+        <span>OJITO_PW</span>
+      </a>
+      <a href="https://www.tiktok.com/@ojito.2021">
+        <img src="/img/icons/tiktok.png" alt="Tiktok de OJITO">
+        <span>ojito.2021</span>
+      </a>
+    </div>
     <a href="https://politicalwatch.es/" class="footer__tenant">
       <img src="/img/politicalwatch.svg" alt="PoliticalWatch">
     </a>
@@ -36,6 +50,25 @@ export default {
     justify-content: space-between;
     padding: 4px 14px;
     opacity: 0.9;
+  }
+
+  &__social {
+    display: flex;
+    padding: 22px 14px 4px;
+    justify-content: space-between;
+    max-width: 320px;
+    margin: 0 auto;
+    a {
+      font-weight: 600;
+      display: inline-block;
+      text-align: center;
+      img {
+        width: 23px;
+        height: auto;
+        margin-right: 4px;
+        margin-top: -3px;
+      }
+    }
   }
 
   &__tenant {
